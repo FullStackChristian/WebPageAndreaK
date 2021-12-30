f1b3dfe4bfcd19595c7e2b395a8a65c97be9dbeb
FullStackChristian <53119854+FullStackChristian@users.noreply.github.com>
2021-12-30 14:20:09 +0000

    adding a slideshow

--- Scripts/script.js
+++ Scripts/script.js
@@ -23,7 +23,7 @@ $().ready(function(){
 
 function formatProductPortfList(prodObj){
 	var ret="";
-	ret=  "<img src="+"'" + prodObj.img + "'"+">";
+	ret=  "<img class='gallery-img' id = '"+prodObj.id+"' src='" + prodObj.img + "' onclick='openSlideShow(this.id)'"+">";
 	return ret;
 }
 
@@ -59,3 +59,41 @@ function validateForm() {
     return false;
   }
 }
+
+//Slideshow code
+
+function openSlideShow(id){
+ //alert("You clicked the element: "+ id);
+ $(".mySlides").show();
+ $(".portfolio-grid").hide();
+ 
+ $.getJSON('Data/productsLarge.json', function(jsonData){
+  var imgItem = jsonData.find(item => item.id === id)
+  document.getElementById("imageid").src=imgItem.img;
+  document.getElementById("imageid").id=imgItem.id;
+  })
+}
+
+function closeSlideShow(){
+  //alert("You clicked the element: "+ id);
+  $(".mySlides").hide();
+  $(".portfolio-grid").show();
+  
+  var currentSlide = document.getElementsByClassName("currentSlide");
+  currentSlide[0].id="imageid";
+  currentSlide[0].src="";
+ }
+
+ function plusSlides(n){
+   var pic = document.getElementsByClassName("currentSlide");
+   var currentId = pic[0].id;
+  
+  $.getJSON('Data/productsLarge.json', function(jsonData){
+    var imgItem = jsonData.find(item => item.id === currentId);
+    var index = jsonData.indexOf(imgItem);
+    var nextItem = jsonData[index+n];
+    var currentSlide = document.getElementsByClassName("currentSlide");
+    currentSlide[0].src=nextItem.img;
+    currentSlide[0].id=nextItem.id;
+  })
+ }
